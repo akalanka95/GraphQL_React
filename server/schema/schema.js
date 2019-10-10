9985d9d463a3b0e759cd37b1b53b463799c46cb4
akalanka <akalankamaduka95@gmail.com>
2019-10-10 09:16:06 +0530

    setting up node js server with graphql

---
+++ server/schema/schema.js
@@ -0,0 +1,38 @@
+const graphql = require('graphql');
+const _ = require('lodash');
+
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+// dumy data 
+
+var books = [
+    { name: 'Name of the wind', genre: 'Fantasy', id: '1' },
+    { name: 'The final empire', genre: 'Fantasy', id: '2' }
+];
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    fields: () => ({
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString }
+    })
+})
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: { id: { type: GraphQLString } },
+            resolve(parent, args) {
+                // code to get data from db/ other source
+                return _.find(books, { id: args.id });
+            }
+        }
+    }
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
