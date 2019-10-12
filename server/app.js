b82c4dc875a7ac8b1a70e98b771386c4c5d5ff83
akalanka <akalankamaduka95@gmail.com>
2019-10-12 10:15:39 +0530

    set the Apolo client with React

--- server/app.js
+++ server/app.js
@@ -2,20 +2,24 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors')
 
-const app  = express();
+const app = express();
+
+// Allow cross-origin requests
+app.use(cors());
 
 mongoose.connect("mongodb+srv://admin:1234@graphqlreact-eswoy.mongodb.net/test?retryWrites=true&w=majority")
-mongoose.connection.once('open',() => {
+mongoose.connection.once('open', () => {
     console.log("connected to database");
 })
 
-app.use('/graphql',graphqlHTTP({
-    schema,
-    graphiql: true
+app.use('/graphql', graphqlHTTP({
+    schema : schema,
+    graphiql: true,
 }));
 
 app.listen(4000, () => {
     console.log('now listening for request on port 4000');
-}); 
+});
 
